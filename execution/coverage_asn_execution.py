89922d28bd4ce9ae2eb4933827aa681875d576da
federicabologna <federica.bologna17@gmail.com>
2022-01-17 15:14:01 -0500

    updating code

--- execution/coverage_asn_execution.py
+++ execution/coverage_asn_execution.py
@@ -1,27 +1,76 @@
 import datetime
-import execution_scripts
 from execution_scripts.initialize_execution import *
 
 
 def coverage_execution(name_cvs_folder, num_cpus):
-
     print(f'EXTRACT META FROM CVs___________{datetime.datetime.now()}')
     cov_dict = adding_metadata(name_cvs_folder)
-    print(datetime.datetime.now())
 
-    cov_years_dict = dict()
     print(f'DOING SEARCHES___________{datetime.datetime.now()}')
-    set_searches(cov_dict, num_cpus, cov_years_dict)
-    print(datetime.datetime.now())
+    set_searches(cov_dict, num_cpus)
+
+    print(f'CALCULATING COVERAGE___________{datetime.datetime.now()}')
+    cov_years_dict = dict()
+    cov_cand_csv = open('cov_asn_xcand.csv', 'w', encoding='utf-8', newline='')
+    cand_cov_writer = csv.writer(cov_cand_csv)
+    cand_cov_writer.writerow(("term", "role", "SA", "field", "field_type", "id", "total_cv",
+                              "MAG", "MAG%", "OA", "OA%", "CR", "CR%",
+                              "MAG+OA", "MAG+OA%", "MAG+CR", "MAG+CR%",
+                              "OA+CR", "OA+CR%", "Comb", "Comb%",))
+
+    cov_dataset_csv = open('cov_asn_xdataset.csv', 'w', encoding='utf-8', newline='')
+    dataset_cov_writer = csv.writer(cov_dataset_csv)
+    dataset_cov_writer.writerow(("dataset", "term", "role", "SA", "field", "field_type", "coverage%"))
+
+    print(f'COV BY CANDIDATE AND DATASET___________{datetime.datetime.now()}')
+    for root, subdirectories, files in os.walk('results'):
+        for filename in files:
+            if 'DS_' not in filename and 'info' not in filename and 'cache' not in filename and 'dict' not in filename:
+                with open(os.path.join(root, filename)) as cand_file:
+                    cand = json.load(cand_file)
+                    cov = {'total_cv': 0, 'mag': 0, 'oa': 0, 'cr': 0, 'comb': 0,
+                           'mag+oa': 0, 'mag+cr': 0, 'oa+cr': 0}
+                    calculating_coverage(cand, cov, cov_years_dict)
+                    term = cand['cv_id'][1]
+                    role = cand['cv_id'][2]
+                    field = cand['cv_id'][3]
+                    field_type = distinction_cd_nd(field)
+                    field_clean = field.replace("-", "/")+f' ({field_type})'
+                    sa = str(field_clean[:2])
+                    cand_id = cand['cv_id'][4]
+                    if cov["total_cv"] > 0:
+                        mag_perc = cov["mag"] / cov["total_cv"]
+                        oa_perc = cov["oa"] / cov["total_cv"]
+                        cr_perc = cov["cr"] / cov["total_cv"]
+                        magoa_perc = cov['mag+oa']/cov['total_cv']
+                        magcr_perc = cov['mag+cr']/cov['total_cv']
+                        oacr_perc = cov['oa+cr']/cov['total_cv']
+                        comb_perc = cov["comb"] / cov["total_cv"]
+
+                        cand_cov_writer.writerow((term, role, sa, field_clean, field_type, cand_id, cov["total_cv"],
+                                                  cov["mag"], mag_perc, cov["oa"], oa_perc, cov["cr"], cr_perc,
+                                                  cov['mag+oa'], magoa_perc, cov['mag+cr'], magcr_perc,
+                                                  cov['oa+cr'], oacr_perc, cov["comb"], comb_perc))
+
+                        dataset_cov_writer.writerow(("MAG", term, role, sa, field_clean, field_type, mag_perc))
+                        dataset_cov_writer.writerow(("OA", term, role, sa, field_clean, field_type, oa_perc))
+                        dataset_cov_writer.writerow(("CR", term, role, sa, field_clean, field_type, cr_perc))
+                        dataset_cov_writer.writerow(("MAG+OA", term, role, sa, field_clean, field_type, magoa_perc))
+                        dataset_cov_writer.writerow(("MAG+CR", term, role, sa, field_clean, field_type, magcr_perc))
+                        dataset_cov_writer.writerow(("OA+CR", term, role, sa, field_clean, field_type, oacr_perc))
+                        dataset_cov_writer.writerow(("Comb", term, role, sa, field_clean, field_type, comb_perc))
+
+    cov_cand_csv.close()
+    cov_dataset_csv.close()
 
     print(f'COV BY YEAR___________{datetime.datetime.now()}')
     sorted_y_dict = sorted(cov_years_dict.items())
-    print(sorted_y_dict)
-    with open('cov_years_asn.csv', 'w', encoding='utf-8', newline='') as cov_csv:
+    with open('cov_asn_xyear.csv', 'w', encoding='utf-8', newline='') as cov_csv:
         writer = csv.writer(cov_csv)
         writer.writerow(("year", "total",
-                         "MAG", "OA", "CR", "combined",
-                         "MAG%", "OA%", "CR%", "comb%"))
+                         "MAG", "MAG%", "OA", "OA%", "CR", "CR%",
+                         "MAG+OA", "MAG+OA%", "MAG+CR", "MAG+CR%",
+                         "OA+CR", "OA+CR%", "Comb", "Comb%"))
         for tuple in sorted_y_dict:
             year = tuple[0]
             info = tuple[1]
@@ -29,34 +78,15 @@ def coverage_execution(name_cvs_folder, num_cpus):
                 mag_perc = info["mag"] / info["total"]
                 oa_perc = info["oa"] / info["total"]
                 cr_perc = info["cr"] / info["total"]
+                magoa_perc = info['mag+oa'] / info['total']
+                magcr_perc = info['mag+cr'] / info['total']
+                oacr_perc = info['oa+cr'] / info['total']
                 comb_perc = info["comb"] / info["total"]
 
                 writer.writerow((year, info["total"],
-                                 info["mag"], info["oa"], info["cr"], info["comb"],
-                                 mag_perc, oa_perc, cr_perc, comb_perc))
-
-    print(f'COV BY CANDIDATE___________{datetime.datetime.now()}')
-    with open('coverage_asn.csv', 'w', encoding='utf-8', newline='') as cov_csv:
-        writer = csv.writer(cov_csv)
-        writer.writerow(("term", "role", "field", "id", "total_cv",
-                         "MAG", "OA", "CR", "combined",
-                         "MAG%", "OA%", "CR%", "comb%"))
-
-        for term, roles in cov_dict['2016'].items():
-            for role, fields in roles.items():
-                for field, cand_ids in fields.items():
-                    for cand_id, cand_dict in cand_ids.items():
-                        if 'cov' in cand_dict.keys():
-                            info = cand_dict["cov"]
-                            if info["total_cv"] > 0:
-                                mag_perc = info["mag"] / info["total_cv"]
-                                oa_perc = info["oa"] / info["total_cv"]
-                                cr_perc = info["cr"] / info["total_cv"]
-                                comb_perc = info["comb"] / info["total_cv"]
-
-                                writer.writerow((term, role, field, cand_id, info["total_cv"],
-                                                 info["mag"], info["oa"], info["cr"], info["comb"],
-                                                 mag_perc, oa_perc, cr_perc, comb_perc))
+                                 info["mag"], mag_perc, info["oa"], oa_perc, info["cr"], cr_perc,
+                                 info["mag+oa"], magoa_perc, info["mag+cr"], magcr_perc,
+                                 info["oa+cr"], oacr_perc, info["comb"], comb_perc))
 
 
 if __name__ == '__main__':
--- execution/execution_scripts/initialize_execution.py
+++ execution/execution_scripts/initialize_execution.py
@@ -1,5 +1,5 @@
 import ray
-from execution_scripts import add_meta
+from execution_scripts.add_meta import *
 from execution_scripts.do_searches import *
 
 
@@ -28,32 +28,7 @@ def adding_metadata(cvs_folder_name):
     return meta_dict
 
 
-def calculating_coverage(info, cov_y_d):
-
-    cov = {'total_cv': 0, 'mag': 0, 'oa': 0, 'cr': 0, 'comb': 0}
-    for pub in info['pubbs']:
-        if pub['year'] not in cov_y_d.keys():
-            cov_y_d[pub['year']] = {'total': 0, 'mag': 0, 'oa': 0, 'cr': 0, 'comb': 0}
-        if 'title' in pub.keys() or 'doi' in pub.keys():
-            cov['total_cv'] += 1
-            cov_y_d[pub['year']]['total'] += 1
-            if 'PId' in pub.keys() or 'oa' in pub.keys() or 'cr' in pub.keys():
-                cov['comb'] += 1
-                cov_y_d[pub['year']]['comb'] += 1
-            if 'PId' in pub.keys():
-                cov['mag'] += 1
-                cov_y_d[pub['year']]['mag'] += 1
-            if 'oa' in pub.keys():
-                cov['oa'] += 1
-                cov_y_d[pub['year']]['oa'] += 1
-            if 'cr' in pub.keys():
-                cov['cr'] += 1
-                cov_y_d[pub['year']]['cr'] += 1
-
-    info['cov'] = cov
-
-
-def set_searches(dd, cpus, cov_y_dict):
+def set_searches(dd, cpus):
     ray.init(num_cpus=cpus, ignore_reinit_error=True)
 
     folder = os.path.join(os.getcwd(), 'results')
@@ -97,7 +72,6 @@ def set_searches(dd, cpus, cov_y_dict):
                         if 'pubbs' in cand_dict.keys():
                             search_in_mag(cand_dict, year_limit, cache)  # search in mag for pubbs and by auids
                             search_oa_cr(cand_dict, cache)  # search in oa, cr and count
-                            calculating_coverage(cand_dict, cov_y_dict)
 
                             with open(cand_file, 'w') as bib_file:
                                 json.dump(cand_dict, bib_file, indent=2)
@@ -106,3 +80,47 @@ def set_searches(dd, cpus, cov_y_dict):
                         with open(cand_file) as bib_file:
                             cand_bib_dict = json.load(bib_file)
                             dd['2016'][term][role][field][cand_id] = cand_bib_dict
+
+
+def calculating_coverage(info, cov, cov_y_d):
+    for pub in info['pubbs']:
+        if pub['year'] not in cov_y_d.keys():
+            cov_y_d[pub['year']] = {'total': 0, 'mag': 0, 'oa': 0, 'cr': 0, 'comb': 0,
+                                    'mag+oa': 0, 'mag+cr': 0, 'oa+cr': 0}
+        if 'title' in pub.keys() or 'doi' in pub.keys():
+            cov['total_cv'] += 1
+            cov_y_d[pub['year']]['total'] += 1
+            if 'PId' in pub.keys() or 'oa' in pub.keys() or 'cr' in pub.keys():
+                cov['comb'] += 1
+                cov_y_d[pub['year']]['comb'] += 1
+            if 'PId' in pub.keys() or 'oa' in pub.keys():
+                cov['mag+oa'] += 1
+                cov_y_d[pub['year']]['mag+oa'] += 1
+            if 'PId' in pub.keys() or 'cr' in pub.keys():
+                cov['mag+cr'] += 1
+                cov_y_d[pub['year']]['mag+cr'] += 1
+            if 'oa' in pub.keys() or 'cr' in pub.keys():
+                cov['oa+cr'] += 1
+                cov_y_d[pub['year']]['oa+cr'] += 1
+            if 'PId' in pub.keys():
+                cov['mag'] += 1
+                cov_y_d[pub['year']]['mag'] += 1
+            if 'oa' in pub.keys():
+                cov['oa'] += 1
+                cov_y_d[pub['year']]['oa'] += 1
+            if 'cr' in pub.keys():
+                cov['cr'] += 1
+                cov_y_d[pub['year']]['cr'] += 1
+
+    return cov
+
+
+def distinction_cd_nd(s):
+    if 1 <= int(s[:2]) <= 9 or s[:4] == "11-E":
+        if int(s[:2]) != 8 or s[-2] == "A" or s[-2] == "B":
+            # if 1 <= int(s[:2]) < 8 or int(s[:2]) == 9 or s[:4] == "11/E" or s[:4] == "08/A" or s[:4] == "08/B":
+            return 'CD'
+        else:
+            return 'ND'
+    else:
+        return 'ND'
